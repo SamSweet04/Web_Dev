8e95742ff107d60d22884ad6e0b7a6f0ca51c6eb
SamSweet04 <s_arystanbek@kbtu.kz>
2023-03-01 02:41:29 +0600

    have changed category image && css

--- lab5/src/app/categories.ts
+++ lab5/src/app/categories.ts
@@ -1,21 +1,26 @@
 export interface Category {
     category: string;
+    image: string;
   }
   
   export const categories = [
     {  
       category: 'All Products',
+      image: 'https://www.couponklub.com/blog/wp-content/uploads/2017/11/136055-gadgets-news-buyer-s-guide-best-8-gadgets-for-entertainment-at-home-this-christmas-image1-vtmahfa47y.jpg',
     },
     {
       category: 'Smartphones',
+      image: 'https://aws.wideinfo.org/shavitech.com/wp-content/uploads/2018/11/08112237/FlipTrends_smartphonesbanner2.jpg',
     },
     {
       category: 'Tablets',
+      image: 'https://prof-krasa.com.ua/wp-content/uploads/2017/05/apple-ipad-2-tech-specs-1.jpg',
     },
     {
       category: 'TV',
+      image: 'https://www.pandashop.md/i/categories/110.jpg',
     },
     {
       category: 'Laptops',
-    }
+      image: 'https://avatars.mds.yandex.net/i?id=673681aaa2b737333971b8e08310b58dc9c4a9fa-8265302-images-thumbs&n=13',    }
   ]
